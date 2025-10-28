7eb9b42e639d46aa84f1ec348e57392c5c3992e4
maniamartial <martialmania19@gmail.com>
2025-10-28 09:51:55 +0300

    fix: import frappe lazily inside console()

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
 
 __version__ = "6.3.0"
 
 
 def console(*data):
+    # Import frappe lazily to avoid import errors during package installation
+    import frappe
     frappe.publish_realtime("toconsole", data, user=frappe.session.user)
--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in posawesome/__init__.py
-from posawesome import __version__ as version
+# get version from __version__ variable in posawesome/__init__.py without importing the package
+version = None
+with open("posawesome/__init__.py", "r") as f:
+    namespace = {}
+    exec(compile(f.read(), "posawesome/__init__.py", "exec"), namespace)
+    version = namespace.get("__version__")
 
 setup(
     name="posawesome",
